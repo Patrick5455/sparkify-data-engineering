e1671804e4c0047bed81d69bd1fce0f14047dbb7
Patrick5455 <patrickojunde@gmail.com>
2020-06-19 09:27:08 +0100

    Refactor Code > Resolve error 'ObjectInUse' where databses could not be dropped becuase another user is connected to the database >  Ran 'sudo service postgresql restart' on terminal to resolve the error > BUg fixed

--- virtual_env_sparkify/.ipynb_checkpoints/create_tables-checkpoint.py
+++ virtual_env_sparkify/.ipynb_checkpoints/create_tables-checkpoint.py
@@ -9,28 +9,39 @@ def create_database():
     - Returns the connection and cursor to sparkifydb
     """
     
+    """
+    Run #sudo service postgresql restart on terminal,
+    if program throws up error on terminal that database could not be dropped 
+    becuase another user is using the session.
+    
+    """
+    
     try:
       # connect to default database
       conn = psycopg2.connect("host=127.0.0.1 dbname=studentdb user=guest password=guest123")
       conn.set_session(autocommit=True)
       cur = conn.cursor()
+      print('connected')
 
       # create sparkify database with UTF8 encoding
       cur.execute("DROP DATABASE IF EXISTS sparkifydb")
+      print("DATABASE DROPED")
       cur.execute("CREATE DATABASE sparkifydb WITH ENCODING 'utf8' TEMPLATE template0")
       print("database created")
 
       # close connection to default database
       conn.close()    
-
       # connect to sparkify database
       conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=guest password=guest123")
       cur = conn.cursor()
       
     except e:
       print("error in creating database")
+      print(e.pgerror)
     
+    #print("cursor and conection returned")
     return cur, conn
+    
 
 
 def drop_tables(cur, conn):
