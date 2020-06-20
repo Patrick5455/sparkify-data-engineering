b531c055dff36f238773bf68991c9b7837da6709
Patrick5455 <patrickojunde@gmail.com>
2020-06-20 09:53:27 +0100

    Refactor SQL Queries

--- virtual_env_sparkify/etl.py
+++ virtual_env_sparkify/etl.py
@@ -80,7 +80,7 @@ def process_data(cur, conn, filepath, func):
 
 
 def main():
-    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=guest password=guest123")
     cur = conn.cursor()
 
     process_data(cur, conn, filepath='data/song_data', func=process_song_file)
--- virtual_env_sparkify/sql_queries.py
+++ virtual_env_sparkify/sql_queries.py
@@ -9,53 +9,101 @@ time_table_drop = "DROP TABLE IF EXISTS time;"
 # CREATE TABLES
 # the SERIAL is use to specify auto increment in postgres and when used autoamtically makes the column NOT NULL and int
 
-songplay_table_create = (""" CREATE TABLE IF NOT EXISTS songplays (songplay_id VARCHAR PRIMARY KEY, start_time int, user_id VARCHAR, level VARCHAR, song_id VARCHAR, artist_id VARCHAR, session_id VARCHAR, location VARCHAR, user_agent VARCHAR);
-""")
+songplay_table_create = (""" CREATE TABLE IF NOT EXISTS songplays (
+                                    songplay_id SERIAL PRIMARY KEY,
+                                    start_time TIMESTAMP,
+                                    user_id VARCHAR,
+                                    level VARCHAR,
+                                    song_id VARCHAR,
+                                    artist_id VARCHAR,
+                                    session_id INTEGER, 
+                                    location VARCHAR,
+                                    user_agent VARCHAR);
+                          """)
 
 # PRIMARY KEY (user_id, first_name)
-user_table_create = (""" CREATE TABLE IF NOT EXISTS users (user_id VARCHAR, first_name VARCHAR, last_name VARCHAR, gender VARCHAR, level VARCHAR);
-""")
+user_table_create = (""" CREATE TABLE IF NOT EXISTS users(
+                          user_id INTEGER PRIMARY KEY,
+                          first_name VARCHAR,
+                          last_name VARCHAR,
+                          gender VARCHAR,
+                          level VARCHAR);
+                          """)
 
-song_table_create = ("""  CREATE TABLE IF NOT EXISTS songs (song_id VARCHAR PRIMARY KEY , title VARCHAR , artist_id VARCHAR , year int, duration float);
-""")
+song_table_create = (""" CREATE TABLE IF NOT EXISTS songs(
+                            song_id VARCHAR PRIMARY KEY ,
+                            title VARCHAR,
+                            artist_id VARCHAR,
+                            year INTEGER,
+                            duration FLOAT);
+                            """)
 
 artist_table_create = (""" CREATE TABLE IF NOT EXISTS artists (
-artist_id VARCHAR PRIMARY KEY, name VARCHAR, location VARCHAR, latitude float, longitude float);
-""")
+                            artist_id VARCHAR PRIMARY KEY,
+                            name VARCHAR,
+                            location VARCHAR,
+                            latitude FLOAT,
+                            longitude FLOAT);
+                            """)
 
-time_table_create = (""" CREATE TABLE IF NOT EXISTS time (start_time int , hour int, day int, week int, month int, year int, weekday int);
-""")
+time_table_create = (""" CREATE TABLE IF NOT EXISTS time 
+                          (start_time TIMESTAMP PRIMARY KEY, 
+                          hour INTEGER,
+                          day INTEGER,
+                          week INTEGER,
+                          month INTEGER,
+                          year INTEGER,
+                          weekday INTEGER);
+                          """)
+
+#________________________________________________________________________________________________
+
 
 # INSERT RECORDS
 
-songplay_table_insert = ("""
-INSERT INTO songplays (songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
-VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s)
+songplay_table_insert = ("""INSERT INTO songplays(
+                              songplay_id,start_time,
+                              user_id, level,
+                              song_id,artist_id,
+                              session_id,location,
+                              user_agent)
+                      VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s)
+                      ON CONFLICT (songplay_id) DO NOTHING;
 """)
 
-song_table_insert = ("""
-INSERT INTO songs (song_id, title, artist_id, year, duration)
-VALUES (%s, %s, %s, %s, %s)
+user_table_insert = (""" INSERT INTO users (
+                            user_id,first_name,
+                            last_name, gender, level)
+                            VALUES(%s, %s, %s, %s, %s)
+                          ON CONFLICT (user_id) DO UPDATE SET level = EXCLUDED.level;
+                          
 """)
 
-artist_table_insert = ("""
-INSERT INTO artists (artist_id, name, location, latitude, longitude)
-VALUES(%s, %s, %s, %s, %s)
+song_table_insert = (""" INSERT INTO songs (
+                            song_id,title,
+                            artist_id,year, duration)
+                      VALUES (%s, %s, %s, %s, %s)
+                      ON CONFLICT DO NOTHING;
+""")
+
+artist_table_insert = (""" INSERT INTO artists 
+                            (artist_id,name,
+                            location, latitude, longitude)
+                      VALUES(%s, %s, %s, %s, %s)
+                      ON CONFLICT DO NOTHING;
 """)
 
 
-time_table_insert = ("""
-INSERT INTO time (start_time, hour, day, week, month, year, weekday)
-VALUES(%s, %s, %s, %s, %s, %s, %s)
+time_table_insert = (""" INSERT INTO time
+                          (start_time, hour,day,
+                          week, month, year, weekday)
+                    VALUES(%s, %s, %s, %s, %s, %s, %s)
+                    ON CONFLICT DO NOTHING;
 """)
 
-user_table_insert = ("""
-INSERT INTO users (user_id, first_name, last_name, gender, level)
-VALUES(%s, %s, %s, %s, %s)
-""")
 # FIND SONGS
 
-song_select = (""" SELECT song_id, artist_id FROM songs JOIN artists ON artist_id
+song_select = (""" SELECT * FROM songs JOIN artists ON artists.artist_id
 """)
 
 # QUERY LISTS
